1ce8a2daeb33c1aea82f8280544518106d7636fe
Kevin Mas <kmas@thess-corp.fr>
2023-01-17 06:58:09 +0100

    feat: adding launches endpoint

---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const { launches } = require("../../models/launches.model");
+const { getAllLaunches } = require("./launches.controller");
+
+const launchesRouter = express.Router();
+
+launchesRouter.get("/launches", getAllLaunches);
+
+module.exports = launchesRouter;
