399cb7bf62eda4f101507af17b0a84262c2f557b
Kevin Mas <kmas@thess-corp.fr>
2023-01-24 08:31:46 +0100

    feat: abort using mongoDB

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,10 +1,15 @@
-const { getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById } = require("../../models/launches.model");
+const {
+	getAllLaunches,
+	existsLaunchWithId,
+	abortLaunchById,
+	scheduleNewLaunch,
+} = require("../../models/launches.model");
 
-function httpGetAllLaunches(req, res) {
-	return res.status(200).json(getAllLaunches());
+async function httpGetAllLaunches(req, res) {
+	return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
 	const launch = req.body;
 
 	if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
@@ -20,21 +25,29 @@ function httpAddNewLaunch(req, res) {
 		});
 	}
 
-	addNewLaunch(launch);
+	await scheduleNewLaunch(launch);
 	return res.status(201).json(launch);
 }
 
-function httpAbortLaunch(req, res) {
+async function httpAbortLaunch(req, res) {
 	const laucnchId = Number(req.params.id);
 
-	if (!existsLaunchWithId(laucnchId)) {
+	const existsLaunch = await existsLaunchWithId(laucnchId);
+
+	if (!existsLaunch) {
 		return res.status(404).json({
 			error: "Launch not found",
 		});
 	}
 
-	const aborted = abortLaunchById(laucnchId);
-	return res.status(200).json(aborted);
+	const aborted = await abortLaunchById(laucnchId);
+	if (!aborted) {
+		return res.status(400).json({
+			error: "Launch not aborted",
+		});
+	}
+
+	return res.status(200).json({ ok: true });
 }
 
 module.exports = {
