f5f2e022f8117e174466330233b44c813e507fb9
Kevin Mas <kmas@thess-corp.fr>
2023-01-26 07:27:21 +0100

    feat: adding .env file

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL = "mongodb+srv://Admin:XmjPj15hPuYqEblw@nasacluster.zth9zql.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
 	console.log("MongoDB: Connection made!");
